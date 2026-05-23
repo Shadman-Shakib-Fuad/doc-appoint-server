8ccf05fd34715d215c8c3897c4862775b1fae091
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-23 18:22:55 +0600

    Fixed render deployment script

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
